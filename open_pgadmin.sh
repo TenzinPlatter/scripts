84b206e7c625bf128e50b870490e2175686d41be
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-05-17 12:24:28 +1000

    Fix pgadmin

--- open_pgadmin.sh
+++ open_pgadmin.sh
@@ -6,7 +6,7 @@ pgadmin_src="/home/tenzin/pgadmin4-9.2"
 if ! tmux has-session -t "pgadmin" 2>/dev/null; then
   echo starting pg_admin...
   tmux new-session -d -s "pgadmin"
-  tmux send-keys -t "pgadmin" "source $pgadmin_src/venv/bin/activate && python3 $pgadmin_src/web/pgAdmin4.py" C-m
+  tmux send-keys -t "pgadmin" "source $pgadmin_src/env/bin/activate && python3 $pgadmin_src/web/pgAdmin4.py" C-m
   sleep 2
 fi
 

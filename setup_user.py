36b448416fb3802477d5104753b0d7327fce3291
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-08-20 23:01:09 +1000

    Auto-commit: modified 1 file

--- setup_user.py
+++ setup_user.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+from typing import NoReturn
+
+import subprocess
+
+def err(msg: str) -> NoReturn:
+    sys.stderr.write(f"[setup_machine]: {msg}")
+    exit(1)
 
 class User:
     def __init__(self, username: str, machine: str):
         self.username = username
         self.machine = machine
 
+    @property
+    def home(self) -> str:
+        return os.path.expanduser(f"~{self.username}")
+
     def __repr__(self):
         return f"{self.username}@{self.machine}"
 
 def main(args: list[str]):
-    remote = User(*args[1:3])
+    try:
+        remote = User(*args[1:3])
+    except IndexError:
+        err("Usage: setup_machine <username> <machine>")
+
+    user = User(os.environ.get("USER", "tenzin"), "localhost")
+
+    if remote.username == user.username and remote.machine == user.machine:
+        err("You are already set up for this machine.")
+
+    
 
 if __name__ == "__main__":
     main(sys.argv)

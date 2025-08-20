693c5fe13db6f91b2b59b9c56d739bf11c719485
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-08-20 22:57:20 +1000

    Auto-commit: modified 1 file

--- setup_user.py
+++ setup_user.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 
+class User:
+    def __init__(self, username: str, machine: str):
+        self.username = username
+        self.machine = machine
+
+    def __repr__(self):
+        return f"{self.username}@{self.machine}"
+
+def main(args: list[str]):
+    remote = User(*args[1:3])
+
+if __name__ == "__main__":
+    main(sys.argv)
